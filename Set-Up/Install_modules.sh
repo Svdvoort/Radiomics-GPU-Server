e11be943e2b3b489daac9d87641cd29ae35d8592
Svdvoort <Svdvoort@users.noreply.github.com>
2018-07-13 18:10:36 +0200

    Added Modules requirements

--- Set-Up/Install_modules.sh
+++ Set-Up/Install_modules.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 git clone --branch v4.1.3 --depth 1 https://github.com/cea-hpc/modules.git /home/admin/Modules
+cd /home/admin/Modules
+
+apt-get install make sed grep cut runtest autoconf automake autopoint gcc tcl-devel
