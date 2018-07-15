638ce6f5809d8e1a3fb1b55e6cfb0d600d016b2c
Svdvoort <Svdvoort@users.noreply.github.com>
2018-07-15 20:29:56 +0200

    Proper set-up of packages

--- First_setup.sh
+++ First_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Make folder for all our installation stuff, we will use admin
-mkdir /home/admin
-cp -R ../Radiomics-GPU-Server /home/admin
-cd /home/admin/Radiomics-GPU-Server
+sudo mkdir /home/admin
+sudo cp -R ../Radiomics-GPU-Server /home/admin
+sudo cd /home/admin/Radiomics-GPU-Server
 
-mkdir /packages
+sudo mkdir /packages
