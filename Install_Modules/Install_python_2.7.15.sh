c05373367597e97d5abb1096089ea51a8ade571b
Svdvoort <Svdvoort@users.noreply.github.com>
2018-07-15 20:37:13 +0200

    Added appropiate sudo commands

--- Install_Modules/Install_python_2.7.15.sh
+++ Install_Modules/Install_python_2.7.15.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Make folder to store temporary files and get source
-mkdir /home/admin/temp_packages/
-mkdir /home/admin/temp_packages/python_2_7_15
+sudo mkdir /home/admin/temp_packages/
+sudo mkdir /home/admin/temp_packages/python_2_7_15
 
 
 cd /home/admin/temp_packages/python_2_7_15
@@ -11,7 +11,7 @@ tar -xzvf Python-2.7.15.tgz
 cd Python-2.7.15
 
 # Configure with the Module folder as target folder
-mkdir /packages/python/2.7.15
+sudo mkdir /packages/python/2.7.15
 ./configure --prefix=/packages/python/2.7.15 --enable-optimizations
 make -j4
-make install
+sudo make install
--- Set-Up/Install_modules.sh
+++ Set-Up/Install_modules.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+sudo mkdir /home/admin/Modules
 git clone --branch v4.1.3 --depth 1 https://github.com/cea-hpc/modules.git /home/admin/Modules
 cd /home/admin/Modules
 
-apt-get install -y make sed grep autoconf automake autopoint gcc tcl-dev dejagnu python
+sudo apt-get install -y make sed grep autoconf automake autopoint gcc tcl-dev dejagnu python
 ./configure --prefix=/usr/share/Modules --modulefilesdir=/etc/modulefiles
 make
-make install
-make distclean
+sudo make install
+sudo make distclean
 
-
-ln -s /usr/share/Modules/init/profile.sh /etc/profile.d/modules.sh
-ln -s /user/share/Modules/init/profile.csh /etc/profile.d/modules.csh
+sudo ln -s /usr/share/Modules/init/profile.sh /etc/profile.d/modules.sh
+sudo ln -s /user/share/Modules/init/profile.csh /etc/profile.d/modules.csh
