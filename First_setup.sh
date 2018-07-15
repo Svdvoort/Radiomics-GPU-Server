3c63390e77793f950857ba1a0bac177525cba1a7
Svdvoort <Svdvoort@users.noreply.github.com>
2018-07-15 20:33:14 +0200

    Extended travis testing to all scripts

--- First_setup.sh
+++ First_setup.sh
@@ -3,6 +3,5 @@
 # Make folder for all our installation stuff, we will use admin
 sudo mkdir /home/admin
 sudo cp -R ../Radiomics-GPU-Server /home/admin
-sudo cd /home/admin/Radiomics-GPU-Server
 
 sudo mkdir /packages
