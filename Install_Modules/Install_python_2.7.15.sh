b10ff385e7792c57519ec5c6a4f21138cd6c0f8b
Svdvoort <Svdvoort@users.noreply.github.com>
2018-07-15 19:11:27 +0200

    Complete install of Modules, start of python Module install

---
+++ Install_Modules/Install_python_2.7.15.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget https://www.python.org/ftp/python/2.7.15/Python-2.7.15.tgz
--- Set-Up/Install_modules.sh
+++ Set-Up/Install_modules.sh
@@ -3,4 +3,12 @@
 git clone --branch v4.1.3 --depth 1 https://github.com/cea-hpc/modules.git /home/admin/Modules
 cd /home/admin/Modules
 
-apt-get install -y make sed grep autoconf automake autopoint gcc tcl-dev dejagnu
+apt-get install -y make sed grep autoconf automake autopoint gcc tcl-dev dejagnu python
+./configure --prefix=/usr/share/Modules --modulefilesdir=/etc/modulefiles
+make
+make install
+make distclean
+
+
+ln -s /usr/share/Modules/init/profile.sh /etc/profile.d/modules.sh
+ln -s /user/share/Modules/init/profile.csh /etc/profile.d/modules.csh
