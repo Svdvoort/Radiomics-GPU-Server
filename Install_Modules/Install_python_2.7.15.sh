5a763d82efb1a187ca0af8ed1c9abbde8356164b
Svdvoort <Svdvoort@users.noreply.github.com>
2018-07-15 22:04:24 +0200

    Added sudo status

--- Install_Modules/Install_python_2.7.15.sh
+++ Install_Modules/Install_python_2.7.15.sh
@@ -7,11 +7,11 @@ sudo mkdir /home/admin/temp_packages/python_2_7_15
 
 cd /home/admin/temp_packages/python_2_7_15
 sudo wget https://www.python.org/ftp/python/2.7.15/Python-2.7.15.tgz
-sudo tar -xzvf Python-2.7.15.tgz
+sudo tar -xzf Python-2.7.15.tgz
 cd Python-2.7.15
 
 # Configure with the Module folder as target folder
 sudo mkdir /packages/python/2.7.15
-./configure --prefix=/packages/python/2.7.15 --enable-optimizations
-make -j4
+sudo ./configure --prefix=/packages/python/2.7.15 --enable-optimizations
+sudo make -j4
 sudo make install
