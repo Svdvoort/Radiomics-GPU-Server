5082302670bf2921f7900d550948c75470d8fde3
Svdvoort <Svdvoort@users.noreply.github.com>
2018-07-15 22:38:58 +0200

    Finalization of python module

--- Install_Modules/Install_python_2.7.15.sh
+++ Install_Modules/Install_python_2.7.15.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+python -V
 # Make folder to store temporary files and get source
 sudo mkdir /home/admin/temp_packages/
 sudo mkdir /home/admin/temp_packages/python_2_7_15
@@ -15,3 +16,12 @@ sudo mkdir /packages/python/2.7.15
 sudo ./configure --prefix=/packages/python/2.7.15 --enable-optimizations
 sudo make -j4
 sudo make install
+
+# Copy modules files to correct directories
+sudo mkdir /etc/modulefiles/python
+sudo cp /home/admin/Radiomics-GPU-Server/Install_Modules/Module_files/Python_2.7.15 /etc/modulefiles/python/2.7.15
+sudo cp /home/admin/Radiomics-GPU-Server/Install_Modules/Module_files/python_version /etc/modulefiles/python/.version
+
+# Check if properly installed
+module load python/2.7.15
+python -V
