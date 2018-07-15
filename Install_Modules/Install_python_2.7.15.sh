066ca29ea63ba1d907382a96f1174eca46f7a18f
Svdvoort <Svdvoort@users.noreply.github.com>
2018-07-15 19:15:18 +0200

    Download python source

--- Install_Modules/Install_python_2.7.15.sh
+++ Install_Modules/Install_python_2.7.15.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+mkdir /home/admin/temp_packages/
+mkdir /home/admin/temp_packages/python_2_7_15
+
+cd /home/admin/temp_packages/python_2_7_15
 wget https://www.python.org/ftp/python/2.7.15/Python-2.7.15.tgz
+tar -xzvf Python-2.7.15.tgz
