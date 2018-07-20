efdcb2e318a51d3d3988af89ef5b9e725412c612
svdvoort <s.vandervoort@erasmusmc.nl>
2018-07-20 16:32:44 +0200

    Updated Python scripts, working tensorflow script

--- Install_Modules/Install_tensorflow.sh
+++ Install_Modules/Install_tensorflow.sh
@@ -3,11 +3,13 @@
 tensorflow_version="1.9.0"
 python_version="2.7.15"
 
+python_tensorflow_directory="/packages/tensorflow/python_${python_version}/${tensorflow_version}"
+
 # Load the modules for which we want to install this tensorflow
 # Start with purge to make sure we have everything we actually want
 source /etc/profile.d/modules.sh
 
-module purge
+module purge all
 module load python/${python_version}
 module load cuda/9.2.148
 module load cudnn/9.2-v7.1
@@ -26,25 +28,34 @@ mkdir -p ${temp_dir}
 cd ${temp_dir}
 
 # Need curl
-# apt-get -y install curl
-#
-# # Start by installing bazel
-# add-apt-repository -y ppa:webupd8team/java
-# apt-get update
-# apt-get -y install oracle-java8-installer
-#
-# echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-# curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
-# apt-get update
-# apt-get -y install bazel
-#
-# # Get python dependencies for tensorflow
-# # Python 2.7
-apt-get install -y python-dev python-pip python-wheel
-/packages/python/${python_version}/bin/pip install numpy# # Python 3
-#apt-get install -y python3-dev python3-pip python3-wheel
-# pip install numpy
-#
+apt-get -y install curl
+
+# Start by installing bazel
+add-apt-repository -y ppa:webupd8team/java
+apt-get update
+apt-get -y install oracle-java8-installer
+
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
+curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
+apt-get update
+apt-get -y install bazel
+
+# Get python dependencies for tensorflow
+python_version_elements=(${python_version//./ })
+python_main_version="${python_version_elements[0]}"
+if [ python_main_version = "2" ]; then
+  # Python 2
+  apt-get install -y python-dev python-pip python-wheel
+  /packages/python/${python_version}/bin/pip install numpy
+  /packages/python/${python_version}/bin/pip install enum34
+  /packages/python/${python_version}/bin/pip install mock
+elif [ python_main_version = "3" ]; then
+  # Python 3
+  apt-get install -y python3-dev python3-pip python3-wheel
+  /packages/python/${python_version}/bin/pip3 install install numpy
+  /packages/python/${python_version}/bin/pip3 install install enum34
+  /packages/python/${python_version}/bin/pip3 install install mock
+
 # # Get tensorflow
 # git clone https://github.com/tensorflow/tensorflow
 cd tensorflow
@@ -71,4 +82,7 @@ echo "If bazel gives an error: Encountered error while reading extension file 'c
 echo "and: All mirrors are down: [java.lang.RuntimeException: Unexpected error: java.security.InvalidAlgorithmParameterException: the trustAnchors parameter must be non-empty]"
 echo "Check out the comments in source for a fix"
 
-bazel build --config=opt //tensorflow/tools/pip_package:build_pip_package
+bazel build --config=opt --verbose_failures //tensorflow/tools/pip_package:build_pip_package
+
+bazel-bin/tensorflow/tools/pip_package/build_pip_package ${python_tensorflow_directory}
+/packages/python/${python_version}/bin/pip install ${python_tensorflow_directory}/*.whl
