58a5c4f06639a30941c00dac003c692d573efe4d
svdvoort <s.vandervoort@erasmusmc.nl>
2018-08-23 17:09:47 +0200

    Solved ssl problem with some python versions

--- Install_Modules/Install_openssl.sh
+++ Install_Modules/Install_openssl.sh
@@ -5,11 +5,11 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-ssl_version = "1.0.1u"
+ssl_version="1.0.1u"
 
-openssl_URL='https://www.openssl.org/source/old/'
+openssl_URL="https://www.openssl.org/source/old"
 main_version=${ssl_version:0:5}
-full_url="${opensll_URL}${main_version}/${ssl_version}"
+full_url="${openssl_URL}/${main_version}/openssl-${ssl_version}.tar.gz"
 mkdir -p /home/admin/temp_packages/openssl/${ssl_version}
 mkdir -p /packages/openssl/${ssl_version}
 cd /home/admin/temp_packages/openssl/${ssl_version}
@@ -17,7 +17,7 @@ wget ${full_url}
 tar -xzf "openssl-${ssl_version}.tar.gz"
 
 cd openssl-${ssl_version}
-./config shared --prefix=/packages/ --openssldir=/packages/openssl/${ssl_version}
+./config shared --prefix=/packages/openssl/${ssl_version} --openssldir=/packages/openssl/${ssl_version}
 make
 make install
 rm -R /home/admin/temp_packages/openssl/${ssl_version}
