3c0ba73f92a2c1724039a080ba688fe318674f31
Svdvoort <Svdvoort@users.noreply.github.com>
2018-08-23 16:50:46 +0200

    updated openssl script

--- Install_Modules/Install_openssl.sh
+++ Install_Modules/Install_openssl.sh
@@ -5,15 +5,19 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-openssl_URL='https://www.openssl.org/source/openssl-1.0.2o.tar.gz'
-mkdir -p /home/admin/temp_packages/openssl-1.0.2
-mkdir -p /packages/openssl1.0/
-cd /home/admin/temp_packages/openssl-1.0.2
-wget ${openssl_URL}
-tar -xzf "openssl-1.0.2o.tar.gz"
+ssl_version = "1.0.1u"
 
-cd openssl-1.0.2o
-./config --openssldir=/packages/openssl1.0/
+openssl_URL='https://www.openssl.org/source/old/'
+main_version=${ssl_version:0:5}
+full_url="${opensll_URL}${main_version}/${ssl_version}"
+mkdir -p /home/admin/temp_packages/openssl/${ssl_version}
+mkdir -p /packages/openssl/${ssl_version}
+cd /home/admin/temp_packages/openssl/${ssl_version}
+wget ${full_url}
+tar -xzf "openssl-${ssl_version}.tar.gz"
+
+cd openssl-${ssl_version}
+./config shared --prefix=/packages/ --openssldir=/packages/openssl/${ssl_version}
 make
 make install
-rm -R /home/admin/temp_packages/openssl-1.0.2
+rm -R /home/admin/temp_packages/openssl/${ssl_version}
