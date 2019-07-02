5898da52f37230c1dd3060a7a63f33e22d7cb3f9
Svdvoort <Svdvoort@users.noreply.github.com>
2019-07-02 15:54:17 +0200

    Update SLURM_script.sh

--- Examples/SLURM_script.sh
+++ Examples/SLURM_script.sh
@@ -10,4 +10,12 @@
 # It will be deleted once your job finishes so don't forget to copy your files!
 MY_TMP_DIR=/slurmtmp/${SLURM_JOB_USER}.${SLURM_JOB_ID}
 
+# Move your data to the folder
+mv <path/to/your/data/> ${MY_TMP_DIR}
+
+# Load the modules
+module purge
+module load python/3.6.7
+module load tensorflow/1.12.0
+
 echo "Hello world"
