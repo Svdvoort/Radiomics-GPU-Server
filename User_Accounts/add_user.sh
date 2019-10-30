b4201ec7c2322a27e0a62b01cd45a05215bed38c
Svdvoort <Svdvoort@users.noreply.github.com>
2019-10-30 14:38:52 +0100

    Make user home on media disk

--- User_Accounts/add_user.sh
+++ User_Accounts/add_user.sh
@@ -9,19 +9,19 @@ fi
 
 # Add user based on first argument, check if sudo on second
 if [ "$2" = "sudo" ]; then
-  adduser "$1" --ingroup researchers
+  adduser -d /media/data/ "$1" --ingroup researchers
   adduser "$1" --ingroup sudo
 else
-  adduser "$1" --ingroup researchers
+  adduser -d /media/data/ "$1" --ingroup researchers
 fi
 
 # Force user to change password on next log-in
 passwd --expire "$1"
 
 # Create a folder for them on the data disk
-mkdir -p "/media/data/${1}"
-chown -R ${1} "/media/data/${1}"
-chmod -R 755 "/media/data/${1}"
+#mkdir -p "/media/data/${1}"
+#chown -R ${1} "/media/data/${1}"
+#chmod -R 755 "/media/data/${1}"
 
 
 # Also add to slurm system
